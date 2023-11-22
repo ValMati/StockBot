b5e60e0d3d6edc64788b0bdefa4812489d32f613
Valentín <v.matilla@protonmail.com>
2023-11-22 20:22:06 +0100

    Complete MessageHandlerTest

--- tests/ValMati.StockBot.Test/Services/MessageHandlerTest.cs
+++ tests/ValMati.StockBot.Test/Services/MessageHandlerTest.cs
@@ -1,6 +1,8 @@
 using Microsoft.Extensions.Logging;
 using NSubstitute;
 using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Requests;
 using Telegram.Bot.Types;
 using ValMati.StockBot.Services;
 
@@ -23,6 +25,91 @@ public class MessageHandlerTest
         await sut.HandleUpdateAsync(botClient, update, CancellationToken.None);
 
         // Assert
-        await botClient.DidNotReceiveWithAnyArgs().SendTextMessageAsync(chatId: default!, text: default!, cancellationToken: default);
+        await botClient
+                .DidNotReceiveWithAnyArgs()
+                .SendTextMessageAsync(chatId: default!, text: default!, cancellationToken: default);
+    }
+
+    [Fact]
+    public async Task HandleUpdateAsync_MessageIsText()
+    {
+        // Arrange
+        ITelegramBotClient botClient = Substitute.For<ITelegramBotClient>();
+
+        Update update = new Update { Message = new Message { Text = null } };
+
+        ILogger<MessageHandler> logger = Substitute.For<ILogger<MessageHandler>>();
+        MessageHandler sut = new MessageHandler(logger);
+
+        // Act
+        await sut.HandleUpdateAsync(botClient, update, CancellationToken.None);
+
+        // Assert
+        await botClient
+                .DidNotReceiveWithAnyArgs()
+                .SendTextMessageAsync(chatId: default!, text: default!, cancellationToken: default);
+    }
+
+    [Fact]
+    public async Task HandleUpdateAsync_OK()
+    {
+        // Arrange
+        Random random = new Random();
+        long chatId = random.NextInt64();
+        string textMessage = Guid.NewGuid().ToString();
+        CancellationToken cancellationToken = new CancellationToken();
+
+        ITelegramBotClient botClient = Substitute.For<ITelegramBotClient>();
+
+        Update update = new Update
+        {
+            Message = new Message
+            {
+                Text = textMessage,
+                Chat = new Chat
+                {
+                    Id = chatId
+                }
+            }
+        };
+
+        ILogger<MessageHandler> logger = Substitute.For<ILogger<MessageHandler>>();
+        MessageHandler sut = new MessageHandler(logger);
+
+        // Act
+        await sut.HandleUpdateAsync(botClient, update, CancellationToken.None);
+
+        // Assert
+        await botClient
+                .Received(1)
+                .MakeRequestAsync(
+                    Arg.Is<SendMessageRequest>(r => r.ChatId == chatId && r.Text.Contains(textMessage)),
+                    cancellationToken);
+    }
+
+    [Fact]
+    public void HandlePollingErrorAsync_LogError()
+    {
+        // Arrange
+        ITelegramBotClient botClient = Substitute.For<ITelegramBotClient>();
+
+        string exceptionMessage = Guid.NewGuid().ToString();
+        ApiRequestException exception = new ApiRequestException(exceptionMessage);
+
+        ILogger<MessageHandler> logger = Substitute.For<ILogger<MessageHandler>>();
+        MessageHandler sut = new MessageHandler(logger);
+
+        // Act
+        sut.HandlePollingErrorAsync(botClient, exception, CancellationToken.None);
+
+        // Assert
+        logger
+            .Received(1)
+            .Log(
+                LogLevel.Error,
+                Arg.Any<EventId>(),
+                Arg.Any<object>(),
+                exception,
+                Arg.Any<Func<object, Exception?, string>>());
     }
 }
