84e3c26a237c39545409247a5889a8e79dd00e56
ValMati <18322719+ValMati@users.noreply.github.com>
2023-11-26 13:35:10 +0100

    Add MockProviderTest

---
+++ tests/ValMati.StockBot.Test/Providers/MockProviderTest.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using ValMati.StockBot.Providers;
+
+namespace ValMati.StockBot.Test.Providers;
+
+public class MockProviderTest
+{
+    [Fact]
+    public async Task GetDataAsync_Ok()
+    {
+        // Arrange
+        MockProvider sut = new();
+
+        // Act
+        var actual = await sut.GetDataAsync(Guid.NewGuid().ToString());
+
+        // Assert
+        actual.Should().NotBeNull();
+    }
+}
